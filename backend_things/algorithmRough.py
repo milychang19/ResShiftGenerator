9ccaf9f7aad88a446aed2730807c9e5fdaae6cc1
Emily Chang <emilychang@Emilys-Air-7.Home>
2023-12-28 00:12:29 -0500

    add residence.csv file

--- backend_things/algorithmRough.py
+++ backend_things/algorithmRough.py
@@ -3,24 +3,31 @@ import csv
 
 #this is staff.csv except in a list of dicts
 testData = [
-    {"numID": 0, "stuID": 86572, "stuName": "emily", "timeOff": [13,14,29,3,10], "PH": 3, "NH": 5},
-    {"numID": 1, "stuID": 75432, "stuName": "richard", "timeOff": [13,14,25,19,9], "PH": 4, "NH": 5},
-    {"numID": 2, "stuID": 23758, "stuName": "molin", "timeOff": [14,16,7,9,23], "PH": 3, "NH": 4},
-    {"numID": 3, "stuID": 76546, "stuName": "drake", "timeOff": [13,5,1,7,28], "PH": 5, "NH": 3},
-    {"numID": 4, "stuID": 65789, "stuName": "cardib", "timeOff": [5,17,18,21,27], "PH": 4, "NH": 3}
+    {"numID": 0, "stuID": 86572, "stuName": "Emily", "timeOff": [13,14,29,3,10], "PH": 3, "NH": 5},
+    {"numID": 1, "stuID": 75432, "stuName": "Richard", "timeOff": [13,14,25,19,9], "PH": 4, "NH": 5},
+    {"numID": 2, "stuID": 23758, "stuName": "Molin", "timeOff": [14,16,7,9,23], "PH": 3, "NH": 4},
+    {"numID": 3, "stuID": 76546, "stuName": "Drake", "timeOff": [13,5,1,7,28], "PH": 5, "NH": 3},
+    {"numID": 4, "stuID": 65789, "stuName": "Cardib", "timeOff": [5,17,18,21,27], "PH": 4, "NH": 3}
 ]
 
-listOfBuildings = [
+residenceBuildings = [
     "LA", "Gordon", "Martime", "Mountain", "Prairie", "Village", "Towers", 
     "Johnston", "Maids", "Mills", "Watson", "Lambton"
 ]
 
 fieldnames = ['numID', 'stuID', 'stuName', 'timeOff', 'PH', 'NH']
-with open("people.csv", mode="w", newline='') as csvFile:
+# Write all members info to file "teamInfo.csv"
+with open("teamInfo.csv", mode="w", newline='') as csvFile:
     CSVdict = csv.DictWriter(csvFile, fieldnames=fieldnames)
     CSVdict.writeheader()
     for emp in testData:
         CSVdict.writerow(emp)
+
+with open("residence.csv", mode="w", newline='') as csvFile:
+    CSVdict = csv.DictWriter(csvFile, fieldnames = fieldnames)
+    CSVdict.writeheader()
+    for building in residenceBuildings:
+        CSVdict.writerow(building)
 
 
 MONTH = 12
@@ -28,7 +35,7 @@ YEAR = 2023
 
 
 
-class algo:
+class algo:# why do we need a algorithm class? I think def would be enough
     def ascendingSort(listOfStaff, shiftType):
         listOfStaff.sort(key=lambda staff: staff[shiftType])
         return listOfStaff
--- backend_things/testFile.py
+++ backend_things/testFile.py
@@ -5,27 +5,5 @@ b = int(input("enter a number: "))
 c = b + a
 print("The \tnumber + 7 is: " + str(c))
 
-print("hello")
-print("omg")
-
-print("Hello World")
-# Example 1: Iterating over a list
-numbers = [1, 2, 3, 4, 5]
-for num in numbers:
-    print(num)
-
-# Example 2: Iterating over a string
-word = "Python"
-for char in word:
-    print(char)
-
-# Example: Using enumerate to get both index and element
-words = ["apple", "banana", "cherry"]
-for index, word in enumerate(words):
-    print(f"Index: {index}, Word: {word}")
-
-for x in range(5):
-    print(x)
-
 import calendar
 print(calendar.monthrange(2023, 12))
