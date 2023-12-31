c619718a7756e2f2ebb65dfbec2d1781ee107adb
Molin Li <molinli660@gmail.com>
2023-12-31 00:19:19 -0500

    FileManager Class

--- backend_things/testFile.py
+++ backend_things/testFile.py
@@ -1,10 +1,3 @@
-print("East is Beast!!!\n")
-
-a = 7
-b = int(input("enter a number: "))
-c = b + a
-print("The \tnumber + 7 is: " + str(c))
-
 import calendar
 print(calendar.monthrange(2023, 12))
 
