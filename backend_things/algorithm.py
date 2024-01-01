8c44620722b73fe0f748f03d604021b7729eca59
Richard Cui <cuir@uoguelph.ca>
2023-12-31 19:07:26 -0500

    algorithm.py

--- backend_things/algorithm.py
+++ backend_things/algorithm.py
@@ -1,55 +1,31 @@
 from calendarDS import calendarDS
-import csv
-import pandas as pd
-import math
 import random
+import sys
 
 MONTH = 12
 YEAR = 2023
 NUMPAIRS = 1
 MAXNUMDAYS = 31
+LIMITONSHIFTS = 200
 
 #this is staff.csv except in a list of dicts
 #fyi try not to delete files until the end bc we need them for version control
-testData = [
-    {"numID": 0, "stuID": 86572, "stuName": "Emily", "timeOff": [13,14,29,3,10], "PH1": 3, "NH2": 5},
-    {"numID": 1, "stuID": 75432, "stuName": "Richard", "timeOff": [13,14,25,19,9], "PH1": 4, "NH2": 5},
-    {"numID": 2, "stuID": 23758, "stuName": "Molin", "timeOff": [14,16,7,9,23], "PH1": 5, "NH2": 4},
-    {"numID": 3, "stuID": 76546, "stuName": "Drake", "timeOff": [13,5,1,7,28], "PH1": 5, "NH2": 3},
-    {"numID": 4, "stuID": 65789, "stuName": "Cardib", "timeOff": [5,17,18,21,27], "PH1": 3, "NH2": 3}
+testData = testData = [
+    {"numID": i,
+     "stuID": random.randint(10000, 99999),
+     "stuName": "Name" + str(i),
+     "timeOff": random.sample(range(1, 32), 5),
+     "PH1": random.randint(3, 5),
+     "NH1": random.randint(3, 5),
+     "PH2": random.randint(3, 5),
+     "NH2": random.randint(3, 5),
+     "BCK": random.randint(3, 5)
+    }
+    for i in range(20)
 ]
 
 #gotta figure out how to dynamically change this depending on team and stuff
-listOfShiftTypes = ["PH1", "NH2"]
-
-staff_df = pd.DataFrame(testData)
-
-
-#avg_ph = math.floor(staff_df.groupby('PH').mean())
-#num = staff_df[staff_df['PH'] > avg_ph]
-
-#LAWA and JAM are the same team, unless we're tryna do something else here??
-residenceBuildings = [
-    "Gordon", "Martime", "Mountain", "Prairie", "Village", "Towers", 
-    "Johnston", "Maids", "Mills", "Watson", "Lambton", "LA"
-]
-
-#fieldnames = ['numID', 'stuID', 'stuName', 'timeOff', 'PH', 'NH']
-# Write all members info to file "teamInfo.csv"
-'''
-with open("teamInfo.csv", mode="w", newline='') as csvFile:
-    CSVdict = csv.DictWriter(csvFile, fieldnames=fieldnames)
-    CSVdict.writeheader()
-    for emp in staff_df:
-        CSVdict.writerow(emp)
-'''
-'''
-with open("residence.csv", mode="w", newline='') as csvFile:
-    CSVdict = csv.DictWriter(csvFile, fieldnames = fieldnames)
-    CSVdict.writeheader()
-    for building in residenceBuildings:
-        CSVdict.writerow(building)
-'''
+listOfShiftTypes = ["PH1", "NH1", "PH2", "NH2", "BCK"]
 
 def fillDaysOffCalendar(listOfStaff, calendar):
     '''
@@ -81,13 +57,17 @@ def findMinShiftCount(listOfStaff, shiftType):
     return minShiftNum
 
 def subarrayOfShiftType(listOfStaff, shiftType, shiftCount):
+    '''
+    Creates a subarray of staff based off of shift type and shift count
+
+    :param listOfStaff: It's the list of dicts that holds info of all the peeps (staff)
+    :param shiftType: The shift type (like 1st pack holder)
+    :param shiftCount: Number of shifts worked/assigned for a specific shift type
+    '''
     subarrayOfStaff = []
     for staff in listOfStaff:
         if staff[shiftType] == shiftCount:
             subarrayOfStaff.append(staff)
-    print(subarrayOfStaff)
-    print(f"Count: {shiftCount}")
-    print(f"Type: {shiftType}")
     return subarrayOfStaff
 
 
@@ -150,11 +130,16 @@ def main():
                     if oopsiePoopsieCounter >= len(subarrayOfStaff):
                         minShiftCount += 1
                         oopsiePoopsieCounter = 0
+                    if minShiftCount == LIMITONSHIFTS:
+                        ds.toString()
+                        print(f"Issue with {dayNum}th. Will need external help")
+                        sys.exit()
     ds.toString()
     for staff in testData:
         print(staff)
     
 main()
+
 
 
 
