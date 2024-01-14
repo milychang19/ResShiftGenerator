5f52af6fc1b04b95cc529ab9b7d9a42f09b6fcc0
Richard Cui <richacui70@gmail.com>
2024-01-14 17:16:20 -0500

    done with pandas

--- ResShiftGenerator/shifts/models.py
+++ ResShiftGenerator/shifts/models.py
@@ -2,20 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Calendar(models.Model):
-    dayNum = models.IntegerField(null=True)
-    emp = models.ForeignKey('Staff', on_delete=models.CASCADE, related_name="assigned", null=True)
-    shiftType = models.CharField(max_length=10)
-    def _str__(self):
-        return f"{self.staff.stuName} {self.shiftType}"
-
-class Staff(models.Model):
-    stuID = models.IntegerField(primary_key=True, unique=True)
-    stuName = models.CharField(max_length = 30)
-    timeOff = models.ManyToManyField(Calendar)
-    numPH1 = models.IntegerField()
-    numNH1 = models.IntegerField()
-    numPH2 = models.IntegerField()
-    numNH2 = models.IntegerField()
-    numBackup = models.IntegerField()
-    def __str__(self):
-        return f"{self.stuID} {self.stuName} PH1 Shift: {self.numPH1} NH1 Shift: {self.numNH1}"
+    SHIFTTYPES = {
+            'ph1': 'Pack Holder #1',
+            'nh1': 'Non Holder #1',
+            'ph2': 'Pack Holder #2',
+            'nh2': 'Non Holder #2',
+            'bck': 'Backup'
+        }
+    dayNum = models.IntegerField()
+    staffID = models.IntegerField()
+    shiftType = models.CharField(max_length=3, choices=SHIFTTYPES)
+    #instance.get_shiftType_display() prints value instead of key of SHIFTTYPES
